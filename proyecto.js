da1fb858b19f3e59df77724760a212305202aadb
fede-Dinuncio <130863043+fede-Dinuncio@users.noreply.github.com>
2024-03-03 21:51:45 +0100

    Update proyecto.js

--- proyecto.js
+++ proyecto.js
@@ -1,16 +1,19 @@
 const Unico_socio = 21524
 let usuarioLogueado = false;
-
-let nombre_unico= "Pedro Gonzalez";
-let nombre_documento= " 30982223";
+let sociosRegistrados = [{
+    nombre: "Pedro Gonzalez",
+    documento: 30982223,
+    numero: 21524
+}];
 
 function ingresarComoSocio (){
 let numeroSocio= prompt ("Ingresa tu numero de socio:") ;
-if (numeroSocio== Unico_socio ){
+let socioEncontrado = sociosRegistrados.find(socio => socio.numero === parseInt(numeroSocio) );
+if (socioEncontrado){
 
     saludar()
 usuarioLogueado = true;
-opcionesLogueado (nombre_unico,nombre_documento,Unico_socio)
+opcionesLogueado (socioEncontrado.nombre, socioEncontrado.documento, socioEncontrado.numero)
 } else {
     alert(" Numero de socio incorrecto");
 }
@@ -29,21 +32,26 @@ ingresarComoSocio();
 }
 }
 function saludar (){
-    console.log ("San Lorenzo, tu Club");
+    alert("San Lorenzo, tu Club");
 }
-function hacerseSocio (){
-    let nombre = prompt ("Ingrese su nombre y apellido");
-    let documento = prompt ("Ingrese su documento");
-    let nuevo_socio = 12345;
-    alert( "Registrado con exito, su numero de socio es " + nuevo_socio);
-    usuarioLogueado = true; 
-    opcionesLogueado (nombre,documento,nuevo_socio) 
+function hacerseSocio() {
+    let nombre = prompt("Ingrese su nombre y apellido");
+    let documento = prompt("Ingrese su documento");
+    let nuevo_socio = {
+        nombre: nombre,
+        documento: documento,
+        numero: Math.floor(Math.random() * (8000 - 1 + 1)) + 1    };
+    sociosRegistrados.push(nuevo_socio); 
+    alert("Registrado con exito, su numero de socio es " + nuevo_socio.numero);
+    usuarioLogueado = true;
+    opcionesLogueado(nuevo_socio.nombre, nuevo_socio.documento, nuevo_socio.numero);
 }
 function opcionesLogueado (nombre,documento,numeroSocio){
     let opcion = prompt ( "1-Ver mis datos 2-Cerrar sesion");
 
     if (opcion ==1) {
         alert("Nombre: " + nombre + " Documento: " + documento + " Número de socio: " + numeroSocio);
+        mostrarSociosRegistrados ();
     }
     else if (opcion ==2){
         usuarioLogueado= false
@@ -54,3 +62,11 @@ function opcionesLogueado (nombre,documento,numeroSocio){
     };
 }   
 
+function mostrarSociosRegistrados (){
+    let listaSocios = "socios registrados";
+    sociosRegistrados.forEach(function(socio) {
+        listaSocios += "Nombre: " + socio.nombre + ", Documento: " + socio.documento + ", Número de socio: " + socio.numero + "\n";
+        
+    });
+    alert (listaSocios);
+}
